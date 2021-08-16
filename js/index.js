a1ca46a4eb7b7641683c3fd4bbaaa3394132be36
teerawat prarom <teewat@yahoo.com>
2021-08-17 00:30:59 +1000

    Completed project

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const baseURL = "http://localhost:3000/";
-  const ul = document.getElementById("list");
   const showPanel = document.getElementById("show-panel");
   const currentUser = 1;
 
   function renderBookList() {
+    const ul = document.getElementById("list");
     fetchBookLists().then((data) => {
       data.forEach((el) => {
         const li = document.createElement("li");
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
         ul.appendChild(li);
         li.addEventListener("click", () => {
           renderBook(el.id);
-          
         });
       });
     });
@@ -34,24 +33,83 @@ document.addEventListener("DOMContentLoaded", function () {
     fetchBook(id).then((data) => {
       clearTags();
       const bookImg = document.createElement("img");
-      const title = document.createElement('h4');
-      const subtitle = document.createElement('h4');
-      const author = document.createElement('h4');
-      const description = document.createElement('p');
+      const title = document.createElement("h4");
+      const subtitle = document.createElement("h4");
+      const author = document.createElement("h4");
+      const description = document.createElement("p");
+
       setAttributes(bookImg, {
         src: data[0].img_url,
         height: "200",
-        width: "120"
+        width: "120",
       });
       title.innerHTML = `<b>${data[0].title}</b>`;
       subtitle.innerHTML = `<b>${data[0].subtitle}</b>`;
       author.innerHTML = `<b>${data[0].author}</b>`;
       description.innerText = data[0].description;
-      
+
       showPanel.append(bookImg, title, subtitle, author, description);
+      renderUserLike(data[0].users, data[0].id);
     });
   }
+
+  function renderUserLike(users, bookID) {
+    const userPanel = document.createElement("ul");
+    const likeBtn = document.createElement("button");
+    let like = false;
+    if (users.length > 0) {
+      users.forEach((el) => {
+        const user = document.createElement("li");
+        user.innerText = el.username;
+        userPanel.appendChild(user);
+      });
+      const currentUserLike = users.find((user) => user.id === currentUser);
+      if (currentUserLike !== undefined) {
+        //current user already liked
+        like = true;
+        likeBtn.innerText = "UNLIKE";
+      } else {
+        like = false;
+        likeBtn.innerText = "LIKE";
+      }
+    } else {
+      //no user like
+      like = false;
+      likeBtn.innerText = "LIKE";
+    }
+    showPanel.append(userPanel, likeBtn);
+    likeBtn.addEventListener("click", () => {
+      if (like) {
+        users = users.filter((user) => user.id !== currentUser);
+        console.log("new user without currentUser: ",users);
+      } else {
+        users.push({
+          id: 1,
+          username: "pouros",
+        });
+        console.log("new user with currentUser: ",users);
+      }
+      fetchLikeFn(users, bookID).then((data) => {
+        console.log("data:  ",data);
+        renderBook(bookID);
+      });
+    });
+  }
+
   //fetches
+
+  function fetchLikeFn(users, id) {
+    confObj = {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: "{ \"users\":"+JSON.stringify(users)+"}"
+    }
+    console.log("confObj: ",confObj);
+    return fetch(baseURL+`books/${id}`,confObj).then((resp) => resp.json());
+  }
+
   function fetchBook(id) {
     return fetch(baseURL + `books?id=${id}`).then((resp) => resp.json());
   }
